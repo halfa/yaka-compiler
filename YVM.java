59f6a3f864a4fa4461f8bc11ab03ae56854c2283
Aurélien Fontaine <aurelien.fontaine@insa-rennes.fr>
2015-03-02 09:32:51 +0100

    Create YVM.java

---
+++ YVM.java
@@ -0,0 +1,46 @@
+public abstract class YVM {
+
+  public iadd();
+  
+  public isub();
+  
+  public imul();
+  
+  public idiv();
+  
+  public inot();
+  
+  public ineg();
+  
+  public iand();
+  
+  public ior();
+  
+  public iinf();
+  
+  public isup();
+  
+  public iinfegal();
+  
+  public isupegal();
+  
+  public iegal();
+  
+  public idiff();
+  
+  public iconst(int valeur);
+  
+  public istore(int offset);
+  
+  public iload(int offset);
+  
+  public ifeq(String etiquette);
+  
+  public iffaux(String etiquette);
+  
+  public goto(String etiquette);
+  
+  public entete();
+  
+  public ouvrePrinc(int var);
+}
