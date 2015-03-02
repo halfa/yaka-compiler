14d6e320eb99fca4413303a4db1b0215c7cbf955
Aurélien Fontaine <aurelien.fontaine@insa-rennes.fr>
2015-03-03 00:31:31 +0100

    Updated YVMasm + .gitingnore
    Eclipse create a .projet which is useless so we don't need to share it !

--- YVM.java
+++ YVM.java
@@ -1,46 +1,48 @@
 public abstract class YVM {
 
-  public void iadd();
-  
-  public void isub();
-  
-  public void imul();
-  
-  public void idiv();
-  
-  public void inot();
-  
-  public void ineg();
-  
-  public void iand();
-  
-  public void ior();
-  
-  public void iinf();
-  
-  public void isup();
-  
-  public void iinfegal();
-  
-  public void isupegal();
-  
-  public void iegal();
-  
-  public void idiff();
-  
-  public void iconst(int valeur);
-  
-  public istore(int offset);
-  
-  public iload(int offset);
-  
-  public ifeq(String etiquette);
-  
-  public iffaux(String etiquette);
-  
-  public goto(String etiquette);
-  
-  public entete();
-  
-  public ouvrePrinc(int var);
+	public void iadd();
+
+	public void isub();
+
+	public void imul();
+
+	public void idiv();
+
+	public void inot();
+
+	public void ineg();
+
+	public void iand();
+
+	public void ior();
+
+	public void iinf();
+
+	public void isup();
+
+	public void iinfegal();
+
+	public void isupegal();
+
+	public void iegal();
+
+	public void idiff();
+
+	public void iconst(int valeur);
+
+	public istore(int offset);
+
+	public iload(int offset);
+
+	public ifeq(String etiquette);
+
+	public iffaux(String etiquette);
+
+	public goto(String etiquette);
+
+	public entete();
+
+	public ouvrePrinc(int var);
+
+	public void queue();
 }
--- YVMasm.java
+++ YVMasm.java
@@ -1,93 +1,156 @@
 public class YVMasm extends YVM {
-  private OutputStream nomFich;
-  
-  public void iadd(){
-    Ecriture.ecrireStringln(nomFich,"pop bx");
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"add ax,bx");
-    Ecriture.ecrireStringln(nomFich,"push bx");
-  }
-  
-  public void isub(){
-    Ecriture.ecrireStringln(nomFich,"pop bx");
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"sub ax,bx");
-    Ecriture.ecrireStringln(nomFich,"push bx");
-  }
-  
-  public void imul(){
-    Ecriture.ecrireStringln(nomFich,"pop bx");
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"imul bx");
-    Ecriture.ecrireStringln(nomFich,"push bx");
-  }
-  
-  public void idiv(){
-    Ecriture.ecrireStringln(nomFich,"pop bx");
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"cwd");
-    Ecriture.ecrireStringln(nomFich,"idiv bx");
-    Ecriture.ecrireStringln(nomFich,"push bx");
-  }
-  
-  public void inot(){}
-  public void ineg(){}
-  public void iand(){
-    Ecriture.ecrireStringln(nomFich,"pop bx");
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"and ax,bx");
-    Ecriture.ecrireStringln(nomFich,"push ax");
-  }
-  
-  public void ior(){
-    Ecriture.ecrireStringln(nomFich,"pop bx");
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"or ax,bx");
-    Ecriture.ecrireStringln(nomFich,"push ax");
-  }
-  
-  public void iinf(){}
-  
-  public void isup(){}
-  
-  public void iinfegal(){
-    Ecriture.ecrireStringln(nomFich,"pop bx");
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"cmp ax,bx");
-    Ecriture.ecrireStringln(nomFich,"jg $+6");
-    Ecriture.ecrireStringln(nomFich,"push -1");
-    Ecriture.ecrireStringln(nomFich,"jmp $+4");
-    Ecriture.ecrireStringln(nomFich,"push 0");
-  }
-  
-  public void isupegal(){}
-  
-  public void iegal(){}
-  
-  public void idiff(){}
-  
-  public void iconst(int valeur){
-    Ecriture.ecrireStringln(nomFich,"push "+valeur);
-  }
-  
-  public void istore(int offset){
-    Ecriture.ecrireStringln(nomFich,"pop ax");
-    Ecriture.ecrireStringln(nomFich,"mov word ptr[bp-"+offset+"],ax");
-  }
-  
-  public void iload(int offset){
-    Ecriture.ecrireStringln(nomFich,"push word ptr [bp-"+offset+"]");
-  }
-  
-  public void ifeq(String etiquette){}
-  
-  public void iffaux(String etiquette){}
-  
-  public void goto(String etiquette){
-     Ecriture.ecrireStringln(nomFich,"jmp "+etiquette);
-  }
-  
-  public void entete(){}
-  
-  public void ouvrePrinc(int var){}
+	private OutputStream nomFich;
+
+	public void iadd(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"add ax,bx");
+		Ecriture.ecrireStringln(nomFich,"push bx");
+	}
+
+	public void isub(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"sub ax,bx");
+		Ecriture.ecrireStringln(nomFich,"push bx");
+	}
+
+	public void imul(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"imul bx");
+		Ecriture.ecrireStringln(nomFich,"push bx");
+	}
+
+	public void idiv(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cwd");
+		Ecriture.ecrireStringln(nomFich,"idiv bx");
+		Ecriture.ecrireStringln(nomFich,"push bx");
+	}
+
+	public void inot(){}
+	public void ineg(){}
+	
+	public void iand(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"and ax,bx");
+		Ecriture.ecrireStringln(nomFich,"push ax");
+	}
+
+	public void ior(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"or ax,bx");
+		Ecriture.ecrireStringln(nomFich,"push ax");
+	}
+
+	public void iinf(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cmp ax,bx");
+		Ecriture.ecrireStringln(nomFich,"jge $+6");
+		Ecriture.ecrireStringln(nomFich,"push -1");
+		Ecriture.ecrireStringln(nomFich,"jmp $+4");
+		Ecriture.ecrireStringln(nomFich,"push 0");
+	}
+
+	public void isup(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cmp bx,ax");
+		Ecriture.ecrireStringln(nomFich,"jge $+6");
+		Ecriture.ecrireStringln(nomFich,"push -1");
+		Ecriture.ecrireStringln(nomFich,"jmp $+4");
+		Ecriture.ecrireStringln(nomFich,"push 0");
+	}
+
+	public void iinfegal(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cmp ax,bx");
+		Ecriture.ecrireStringln(nomFich,"jg $+6");
+		Ecriture.ecrireStringln(nomFich,"push -1");
+		Ecriture.ecrireStringln(nomFich,"jmp $+4");
+		Ecriture.ecrireStringln(nomFich,"push 0");
+	}
+
+	public void isupegal(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cmp bx,ax");
+		Ecriture.ecrireStringln(nomFich,"jg $+6");
+		Ecriture.ecrireStringln(nomFich,"push -1");
+		Ecriture.ecrireStringln(nomFich,"jmp $+4");
+		Ecriture.ecrireStringln(nomFich,"push 0");
+	}
+
+	public void iegal(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cmp ax,bx");
+		Ecriture.ecrireStringln(nomFich,"jne $+6");
+		Ecriture.ecrireStringln(nomFich,"push -1");
+		Ecriture.ecrireStringln(nomFich,"jmp $+4");
+		Ecriture.ecrireStringln(nomFich,"push 0");
+	}
+
+	public void idiff(){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cmp ax,bx");
+		Ecriture.ecrireStringln(nomFich,"je $+6");
+		Ecriture.ecrireStringln(nomFich,"push -1");
+		Ecriture.ecrireStringln(nomFich,"jmp $+4");
+		Ecriture.ecrireStringln(nomFich,"push 0");
+	}
+
+	public void iconst(int valeur){
+		Ecriture.ecrireStringln(nomFich,"push "+valeur);
+	}
+
+	public void istore(int offset){
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"mov word ptr[bp-"+offset+"],ax");
+	}
+
+	public void iload(int offset){
+		Ecriture.ecrireStringln(nomFich,"push word ptr [bp-"+offset+"]");
+	}
+
+	public void ifeq(String etiquette){
+		Ecriture.ecrireStringln(nomFich,"pop bx");
+		Ecriture.ecrireStringln(nomFich,"pop ax");
+		Ecriture.ecrireStringln(nomFich,"cmp ax,bx");
+		Ecriture.ecrireStringln(nomFich,"je "+etiquette);
+	}
+
+	public void iffaux(String etiquette){
+		
+	}
+
+	public void goto(String etiquette){
+		Ecriture.ecrireStringln(nomFich,"jmp "+etiquette);
+	}
+
+	public void entete(){
+		Ecriture.ecrireStringln(nomFich,".model SMALL");
+		Ecriture.ecrireStringln(nomFich,".586");
+		Ecriture.ecrireStringln(nomFich,".CODE");
+		Ecriture.ecrireStringln(nomFich,"debut :");
+		Ecriture.ecrireStringln(nomFich,"	STARTUPCODE");
+	}
+
+	public void ouvrePrinc(int var){
+		Ecriture.ecrireStringln(nomFich,"mov bp,sp");
+		Ecriture.ecrireStringln(nomFich,"sub sp,"+var);
+	}
+
+	public void queue(){
+		Ecriture.ecrireStringln(nomFich,"nop");
+		Ecriture.ecrireStringln(nomFich,"exitcode");
+		Ecriture.ecrireStringln(nomFich,"end debut");
+	}
 }
