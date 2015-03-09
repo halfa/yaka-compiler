97e1f69fb3fbc407978691e2f946502760abe09f
Thomas Hareau <thomashareau@free.fr>
2015-03-09 11:30:39 +0100

    Idvar test with offset check

---
+++ test/IdVarTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class IdVarTest {
+
+	
+	@Test
+	public void testOffset() {
+		IdVar i1= new IdVar('a', Type.INTEGER);
+		IdVar i2= new IdVar('a', Type.INTEGER);
+		
+		assertEquals(i1.getOffset(),-2);		
+		assertEquals(i2.getOffset(),-4);
+		
+		i1.setVal('3');
+		assertEquals(i1.getOffset(),-2);		
+		
+		
+	}
+
+}
