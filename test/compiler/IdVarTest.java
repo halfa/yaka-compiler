c12f31b44751eaaa4e37dbff14b1fcd8d4ef74f4
Thomas Hareau <thomashareau@free.fr>
2015-03-16 17:24:55 +0100

    update toward idvar change

--- test/compiler/IdVarTest.java
+++ test/compiler/IdVarTest.java
@@ -12,13 +12,17 @@ public class IdVarTest {
 	
 	@Test
 	public void testOffset() {
-		IdVar i1= new IdVar('a', Type.INTEGER);
-		IdVar i2= new IdVar('a', Type.INTEGER);
+		IdVar i1= new IdVar(Type.INTEGER);
+		IdVar i2= new IdVar(Type.INTEGER);
+		
+		TabIdent t = new TabIdent();
+		t.addIdent("a", i1);
+		t.addIdent("a", i2);
 		
 		assertEquals(i1.getOffset(),-2);		
 		assertEquals(i2.getOffset(),-4);
 		
-		i1.setVal('3');
+		t.addIdent("3",i1);
 		assertEquals(i1.getOffset(),-2);		
 		
 		
