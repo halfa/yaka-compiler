3a23eb7994164751dab4cfb01b758027f64ac4ae
halfa <youngyukari@gmail.com>
2015-03-16 09:25:31 +0100

    Global and local ident for functions

--- src/compiler/TabIdent.java
+++ src/compiler/TabIdent.java
@@ -5,13 +5,15 @@ import java.util.HashMap;
  */
 public class TabIdent {
 	
-	static private HashMap<String, Ident> table = new HashMap<String, Ident>();
+	static private HashMap<String, Ident> global = new HashMap<String, Ident>();
+	// TODO implémenter la priorité local/globale dans les getSet
+	static private HashMap<String, Ident> local = new HashMap<String, Ident>();
 	
-	static Ident getIdent(String clef) { return table.get(clef); }
-	static void addIdent(String clef, Ident id){ table.put(clef, id); }
+	static Ident getIdent(String clef) { return global.get(clef); }
+	static void addIdent(String clef, Ident id){ global.put(clef, id); }
 	
 	static boolean existeIdent(String clef) {
-		return table.containsKey(clef);
+		return global.containsKey(clef);
 	}
 
 }
