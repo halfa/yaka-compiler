5c368c036a3ab75a354f9cd762fabd44884b139e
Thomas Hareau <thomashareau@free.fr>
2015-03-16 09:41:33 +0100

    indentation

--- src/compiler/TabIdent.java
+++ src/compiler/TabIdent.java
@@ -9,18 +9,19 @@ import exception.UnknownIdentException;
  */
 public class TabIdent {
 	static private HashMap<String, Ident> global = new HashMap<String, Ident>();
-static private HashMap<String, Ident> local = new HashMap<String, Ident>();
-
+	static private HashMap<String, Ident> local = new HashMap<String, Ident>();
 
 	static Ident getIdent(String clef) throws UnknownIdentException {
 		Ident i = global.get(clef);
-		if(i==null)throw new UnknownIdentException(clef);
+		if (i == null)
+			throw new UnknownIdentException(clef);
 		return i;
 	}
 
 	static void addIdent(String clef, Ident id) {
 		global.put(clef, id);
 	}
+
 	static boolean existeIdent(String clef) {
 		return global.containsKey(clef);
 	}
