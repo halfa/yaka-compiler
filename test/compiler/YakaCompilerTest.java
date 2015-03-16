09dceccac7be079d9bbd281782b185d357ba933a
Thomas Hareau <thomashareau@free.fr>
2015-03-16 15:55:31 +0100

    Creation of JUnit test for check validitate of previusly implemented function

---
+++ test/compiler/YakaCompilerTest.java
@@ -0,0 +1,19 @@
+package compiler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class YakaCompilerTest {
+
+	
+	@Test
+	public void maxTest() {
+		String[] args = {"max.yak"};
+		Yaka.main(args);
+		
+		
+	}
+
+}
