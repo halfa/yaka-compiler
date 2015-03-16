c2f0bbc62229ac9150c79972bb9377b71069b4d0
halfa <youngyukari@gmail.com>
2015-03-16 19:52:32 +0100

    Implement functions

---
+++ src/compiler/IdFun.java
@@ -0,0 +1,64 @@
+package compiler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Gestion des fonctions, de leur type et paramètres
+ *
+ */
+public class IdFun extends Ident {
+
+	private List<Parameter> parameters; 
+	
+	/****************************************
+	 * Sous-classe pour gérer les paramètres.
+	 ****************************************/
+	class Parameter {
+		String name;
+		IdVar id;
+		
+		/* Constructeur */
+		public Parameter(String n, IdVar p) {
+			name = n;
+			id = p;
+		}
+		
+		/* GetSet */
+		public String getName(){ return name; }
+		public IdVar getIdent(){ return id; }
+
+		
+		public String toString(){
+			return name+" "+id;
+		}
+		
+	}
+	
+	// Contructeur de fonction sans paramètre
+	public IdFun(Type t) {
+		super(t); // type de retour
+		parameters =  new ArrayList<Parameter>();
+	}
+	
+	public List<Parameter> getParameters(){
+		return parameters;
+	}
+	/**
+	 * Ajoute un paramètre à la fonction
+	 */
+	public void addParameter(String name, IdVar p){
+		parameters.add(new Parameter(name, p));
+	}
+	
+	/**
+	 * String output for debug purpose
+	 */
+	public String toString(){
+		String s = new String("FONCTION\n");
+		for (Parameter i : parameters) {
+			s += i;
+		}
+		return s;
+	}
+}
