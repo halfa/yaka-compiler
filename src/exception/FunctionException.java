812e4f1f6f154d127d9562633f0f3fe1de54d73c
olivier-dj <olivierdejonckere@gmail.com>
2015-03-31 14:24:13 +0200

    current function name added the exception string message

--- src/exception/FunctionException.java
+++ src/exception/FunctionException.java
@@ -1,9 +1,11 @@
 package exception;
 
+import compiler.Declaration;
+
 public class FunctionException extends YakaException {
 
 	public FunctionException(String string) {
-		super("in Function :"+string);
+		super("in function "+Declaration.getNameCurrentFunction()+ " : "+string);
 		// TODO Auto-generated constructor stub
 	}
 
