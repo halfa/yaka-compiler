0bced19f511fd538c847b610016d15bff56927c2
olivier-dj <olivierdejonckere@gmail.com>
2015-04-03 11:23:28 +0200

    add line in YakaException.java

--- src/exception/YakaException.java
+++ src/exception/YakaException.java
@@ -1,9 +1,13 @@
 package exception;
 
+import compiler.Yaka;
+
 public class YakaException extends Exception {
 
+	int line;
 	public YakaException(String string) {
-		super(string);
+		super("At line"+Yaka.token.beginLine+" : "+string);
+		
 	}
 
 }
